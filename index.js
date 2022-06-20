fdf98039b2b8750b04e9846011c6c6b3bc6fbc4b
Ashley <ashleyeffenberger@gmail.com>
2022-06-20 16:40:33 +0200

    anfang

---
+++ index.js
@@ -0,0 +1,14 @@
+const player = {
+  name: "Edgar",
+  points: 100
+} 
+
+const btnPositiv = document.querySelector("#positive")
+const btnNeutral = document.querySelector("#neutral")
+const btnNegativ = document.querySelector("#negative")
+
+let positive = 5;
+let neutral = 0;
+let negative = -5;
+
+console.log(player.points+positive);
