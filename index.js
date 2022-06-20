562f72bb1536df7c08ea1f233e36d774ef36eb35
Ashley <ashleyeffenberger@gmail.com>
2022-06-20 17:04:28 +0200

    plus funktion

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
+//Spieler
+
 const player = {
   name: "Edgar",
   points: 100
 } 
 
-const btnPositiv = document.querySelector("#positive")
-const btnNeutral = document.querySelector("#neutral")
-const btnNegativ = document.querySelector("#negative")
+//Antworten auf Spieler Reaktion
+const question=[
+  {antwortPos: "lorem", antwortNeu: "lorem Neutral",antwortNegativ: "meh"},
+  {antwortPos: "lorem", antwortNeu: "lorem Neutral",antwortNegativ: "meh"},
+  {antwortPos: "lorem", antwortNeu: "lorem Neutral",antwortNegativ: "meh"},
+  {antwortPos: "lorem", antwortNeu: "lorem Neutral",antwortNegativ: "meh"},
+  {antwortPos: "lorem", antwortNeu: "lorem Neutral",antwortNegativ: "meh"},
+  {antwortPos: "lorem", antwortNeu: "lorem Neutral",antwortNegativ: "meh"},
+  {antwortPos: "lorem", antwortNeu: "lorem Neutral",antwortNegativ: "meh"},
+  {antwortPos: "lorem", antwortNeu: "lorem Neutral",antwortNegativ: "meh"},
+  {antwortPos: "lorem", antwortNeu: "lorem Neutral",antwortNegativ: "meh"},
+]
 
+//Buttons
+const btnPositiv = document.querySelector("#positive");
+const btnNeutral = document.querySelector("#neutral");
+const btnNegativ = document.querySelector("#negative");
+const btnSubmit = document.querySelector("#answer");
+btnNegativ.addEventListener("click", next)
+
+// Punke Sytem
 let positive = 5;
 let neutral = 0;
 let negative = -5;
 
-console.log(player.points+positive);
+//Funktion die Punkte zuordnet
+function next (e){
+if (btnPositiv.checked === true) {
+  player.points + positive;
+} else if (btnNegativ.checked === true){
+  player.points + negative;
+}
+return player.points
+}
+
+//nächste Frage
