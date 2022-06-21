6bc164f84c1bb664369fbd6c79085f6741e4605a
Ashley <ashleyeffenberger@gmail.com>
2022-06-21 11:39:54 +0200

    punkte system hergestellt +  to do liste

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # game
+
+## To Do
+
+1. Spieler Konstruktion --> Name --> Startbildschirm
+
+2.a) Funktion Merlin neuer Text
+
+2 b) weiter Auffangen von Merlins weiter
+
+
+3. Funktion neue Antowrtmöglichkeiten für Spieler
+
+4. Antworten auswerten
+
+5. Bildschirm für Ende
+
+##
+Spiel Ablauf:
+
+Spieler Name eingeben(Bild vom Laden)
+
+Spieler ist IM laden(Bild von Merlin)
+-Merlin begrüßt Spieler
+->Spieler kann unmittelbar antworten(es gibt immer eine positive, negative und neutrale möglichkeit)
+
+-Merlin reagiert positiv,neutral oder negativ
+->Spieler kann auf Weiter klicken
+
+-Merlin sagt etwas
+->Spieler kann unmittelbar amtworten
+
+-Merlin reagoiert wieder(alles wiederholt sich)
+
+Ende:
+Punkanzahl soll zusammen gerechnet werden
+(Negative antworten ziehen punkte ab, positive antworten addieren Punkte und beu neutralen passiert nichts)
+
+Nach ablauf der Runden passiert folgendes(gerechnet wird erstmal mit 20 runden, Spieler startet mit 100 Punkten):
+
+Negativ Ende: text plus Bild(0-100 Punkte)
+Neutral Ende: text plus Bild(105-145 Punkte)
+Positiv Ende: text plus bild(150-200 Punkte)
--- index.js
+++ index.js
@@ -2,42 +2,114 @@
 
 const player = {
   name: "Edgar",
-  points: 100
+  points: 100,
+  round: 0,
 } 
+
+//Merlins Texte
+const merlin=[
+  {reaction: ""},
+  {reaction: ""},
+  {reaction: ""},
+  {reaction: ""},
+  {reaction: ""},
+  {reaction: ""},
+  {reaction: ""},
+  {reaction: ""},
+]
+
 
 //Antworten auf Spieler Reaktion
 const question=[
-  {antwortPos: "lorem", antwortNeu: "lorem Neutral",antwortNegativ: "meh"},
-  {antwortPos: "lorem", antwortNeu: "lorem Neutral",antwortNegativ: "meh"},
-  {antwortPos: "lorem", antwortNeu: "lorem Neutral",antwortNegativ: "meh"},
-  {antwortPos: "lorem", antwortNeu: "lorem Neutral",antwortNegativ: "meh"},
-  {antwortPos: "lorem", antwortNeu: "lorem Neutral",antwortNegativ: "meh"},
-  {antwortPos: "lorem", antwortNeu: "lorem Neutral",antwortNegativ: "meh"},
-  {antwortPos: "lorem", antwortNeu: "lorem Neutral",antwortNegativ: "meh"},
-  {antwortPos: "lorem", antwortNeu: "lorem Neutral",antwortNegativ: "meh"},
-  {antwortPos: "lorem", antwortNeu: "lorem Neutral",antwortNegativ: "meh"},
+  {antwortPos: "pos1", antwortNeu: "neu1",antwortNegativ: "neg1"},
+  {antwortPos: "pos2", antwortNeu: "neu2",antwortNegativ: "neg2"},
+  {antwortPos: "pos3", antwortNeu: "neu3",antwortNegativ: "neg3"},
+  {antwortPos: "pos4", antwortNeu: "neu4",antwortNegativ: "neg4"},
+  {antwortPos: "pos5", antwortNeu: "neu5",antwortNegativ: "neg5"},
+  {antwortPos: "pos6", antwortNeu: "neu6",antwortNegativ: "neg6"},
+  {antwortPos: "pos7", antwortNeu: "neu7",antwortNegativ: "neg7"},
+  {antwortPos: "pos8", antwortNeu: "neu8",antwortNegativ: "neg8"},
+  {antwortPos: "pos9", antwortNeu: "neu9",antwortNegativ: "neg9"},
 ]
 
 //Buttons
-const btnPositiv = document.querySelector("#positive");
-const btnNeutral = document.querySelector("#neutral");
-const btnNegativ = document.querySelector("#negative");
+//brauch ich nicht mehr?
+const form = document.querySelector('#form')
+
+const antwortBox = document.querySelector('.antwortBox')
+
+//brauch ich nicht mehr?
 const btnSubmit = document.querySelector("#answer");
-btnNegativ.addEventListener("click", next)
+
+const merlinText = document.querySelector('.merlin')
+
 
 // Punke Sytem
 let positive = 5;
 let neutral = 0;
 let negative = -5;
 
-//Funktion die Punkte zuordnet
-function next (e){
-if (btnPositiv.checked === true) {
-  player.points + positive;
-} else if (btnNegativ.checked === true){
-  player.points + negative;
-}
-return player.points
+//Funktion die Punkte zuordnet, je nach dem was wir geantwortet haben
+function next (antwort){
+console.log('Antwort des Spielers: ', antwort);
+  if ("antwortPos" === antwort) {
+  player.points += positive;
+} else if ("antwortNegativ" === antwort){
+  player.points += negative;
+} 
+player.round++
+console.log("Runde ist:"+player.round)
+console.log("Punkte sind:"+player.points)
+newQuestion()
+// return player.points
 }
 
-//nächste Frage
+//Merlins neuer Text
+function clearMerlin(){
+  merlinText.innerHTML= '<p></p>'
+}
+
+//In der funktion soll Merlins text aus der const genommen werden
+function newMerlinText(){
+  merlinText.innerHTML= '<p>Ich bin neuer Text</p>'
+}
+
+//nächste Frage des Spielers und neuer Melrin text?
+function newQuestion(){
+  clearMerlin()
+  antwortBox.innerHTML = `
+  <button class="antwortBtn" onclick="checkAnswer(this)">
+  ${question[player.round].antwortPos}
+  </button>
+  <button class="antwortBtn" onclick="checkAnswer(this)">
+  ${question[player.round].antwortNeu}
+  </button>
+  <button class="antwortBtn" onclick="checkAnswer(this)">
+  ${question[player.round].antwortNegativ}
+  </button>
+  `
+  newMerlinText()
+}
+
+//funktion die schaut was der Spieler geantwortet hat
+function checkAnswer(e){
+  let answer = e.innerText
+  console.log('Unsere Antwort: ',answer);
+  //Check if neutral or pos ...
+  //such den Value und sag mir den key
+  for (const elem in question[player.round]){
+    if(question[player.round][elem] === answer ){
+      next(elem)
+    }
+  }
+}
+
+
+
+answer.onclick = () => {
+  newQuestion()
+}
+
+
+
+
